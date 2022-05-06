011793cc3b1afda24d73e04f12d55f84bbbd533d
yazhao.hou <yazhao.hou@upai.com>
2022-05-06 15:34:10 +0800

    feat

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
-let isHas = [1, 2, 3].includes(2)
+// class Dong {}
 
-new Promise((resolve, reject) => {
-  resolve(100)
-})
-
-// es6+ 源码：
-// const asyncFun = async () => {
-//   await new Promise(setTimeout, 2000)
-
-//   return '2s 延时后返回字符串'
+// async function func() {
 // }
-// export default asyncFun
+new WeakMap()
